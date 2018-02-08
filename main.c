716f4c9a1e4cd01fd949421b6dd046a60ec74601
Bluecoreg <36262058+Bluecoreg@users.noreply.github.com>
2018-02-08 13:03:23 -0800

    Added main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+filename    main.c
+author      Bluecore
+git-hub     https://github.com/Bluecoreg/C-Programming
+GCC         gcc -Werror -O3 -Wall -Wextra -ansi -pedantic main.c -o main
+
+Brief Description:
+  This program displays information on the screen. 
+******************************************************************************/
+#include <stdio.h> /* printf */
+
+/* Display information on the screen */
+int main(void)
+{
+  printf("Hello World!\n");
+  
+    /* Return successful value to OS */
+  return 0;
+}
